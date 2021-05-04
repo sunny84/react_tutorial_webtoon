a6005395585101fc0d8f7504aef592fdf183acb4
SunHee <sonya8412@gmail.com>
2021-05-05 00:04:38 +0900

    공통 컴포넌트 만들기

---
+++ src/component/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = () => (
+    <div className="footer">
+        ⓒ My Company Name.
+    </div>
+)
+
+export default Footer;
---
+++ src/component/Gnb.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Gnb = () => (
+    <ul className="gnb">
+        <li>
+            <a href="#none" className="tab_day on">월요일</a>
+        </li>
+        <li>
+            <a href="#none" className="tab_day">화요일</a>
+        </li>
+        <li>
+            <a href="#none" className="tab_day">수요일</a>
+        </li>
+        <li>
+            <a href="#none" className="tab_day">목요일</a>
+        </li>
+        <li>
+            <a href="#none" className="tab_day">금요일</a>
+        </li>
+        <li>
+            <a href="#none" className="tab_day">토요일</a>
+        </li>
+        <li>
+            <a href="#none" className="tab_day">일요일</a>
+        </li>
+    </ul>
+)
+
+export default Gnb;
