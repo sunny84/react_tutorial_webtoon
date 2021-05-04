362560989d4a2db216556c145ff7fc4eac99c432
SunHee <sonya8412@gmail.com>
2021-05-04 23:48:40 +0900

    라우터 적용, 웹툰서비스 만들기

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Main from './container/Main';
+import Viewer from './container/Viewer';
+import WebtoonHome from './container/WebtoonHome';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Route exact path="/" component={Main} />
+          <Route path="/webtoon/:webtoonId" component={WebtoonHome} />
+          <Route path="/viewer/:episodeld" component={Viewer} />
+        </div>
+      </Router>
+    )
+  }
 }
 
 export default App;
---
+++ src/container/Viewer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Main from './Main';
+
+class Viewer extends Component{
+    render(){
+        return (
+            <div>Viewer</div>
+        )
+    }
+}
+
+export default Main;
