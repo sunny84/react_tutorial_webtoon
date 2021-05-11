a444f009de04bd9ab404cd6b7aea1664d3feb8e5
SunHee <sonya8412@gmail.com>
2021-05-11 17:09:32 +0900

    GNB ㅓ리

--- src/component/Gnb.js
+++ src/component/Gnb.js
@@ -1,27 +1,28 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
-const Gnb = () => (
+const Gnb = (props) => (
     <ul className="gnb">
         <li>
-            <a href="#none" className="tab_day on">월요일</a>
+            <Link to="/?day=mon" className={ props.day === 'mon' ? 'tab_day on' : 'tab_day'}>월요일</Link>
         </li>
         <li>
-            <a href="#none" className="tab_day">화요일</a>
+            <Link to="/?day=tue" className={ props.day === 'tue' ? 'tab_day on' : 'tab_day'}>화요일</Link>
         </li>
         <li>
-            <a href="#none" className="tab_day">수요일</a>
+            <Link to="/?day=wed" className={ props.day === 'wed' ? 'tab_day on' : 'tab_day'}>수요일</Link>
         </li>
         <li>
-            <a href="#none" className="tab_day">목요일</a>
+            <Link to="/?day=thu" className={ props.day === 'thu' ? 'tab_day on' : 'tab_day'}>목요일</Link>
         </li>
         <li>
-            <a href="#none" className="tab_day">금요일</a>
+            <Link to="/?day=fri" className={ props.day === 'fri' ? 'tab_day on' : 'tab_day'}>금요일</Link>
         </li>
         <li>
-            <a href="#none" className="tab_day">토요일</a>
+            <Link to="/?day=sat" className={ props.day === 'sat' ? 'tab_day on' : 'tab_day'}>토요일</Link>
         </li>
         <li>
-            <a href="#none" className="tab_day">일요일</a>
+            <Link to="/?day=sun" className={ props.day === 'sun' ? 'tab_day on' : 'tab_day'}>일요일</Link>
         </li>
     </ul>
 )
